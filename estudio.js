7416a1282ad6939c7ffc1a2c1ae3fa1a4a057646
Ivo <i.gozze@hotmail.com>
2024-05-06 19:24:19 -0400

    Insertar forEach para iterar arreglo

--- estudio.js
+++ estudio.js
@@ -1,5 +1,14 @@
 const numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,8,9,10];
 
-const encRepetidos = (arreglo) =>{
+const encRepetidos = (arr) =>{
+  let contar = []
 
-}
+  arr.forEach(num => {
+
+    contar.push(num)
+  });
+
+  return contar
+};
+
+console.log(encRepetidos(numeros))
