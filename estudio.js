5418c8afb6317e4f14e0100bc183fd2f18d8db58
Ivo <i.gozze@hotmail.com>
2024-05-06 19:56:17 -0400

    Implementacion encontrar todos los numeros que se repiten

--- estudio.js
+++ estudio.js
@@ -1,14 +1,24 @@
 const numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,8,9,10];
 
 const encRepetidos = (arr) =>{
-  let contar = []
+  let contar = [];
 
-  arr.forEach(num => {
+  const numeros = {
+    repetidos:[]
+  }
 
-    contar.push(num)
-  });
+  for(i = 0; i<arr.length ; i++){
+    if(contar.includes(arr[i])){
 
-  return contar
+      numeros.repetidos.push(arr[i]);
+
+    }
+    else{
+      contar.push(arr[i]);
+    }
+  };
+
+  return numeros
 };
 
 console.log(encRepetidos(numeros))
