ea82afc30ebc17cb5736c8847bd4966f48d09f75
Ivo <i.gozze@hotmail.com>
2024-05-08 19:38:20 -0400

    se cambio nombre de objeto numeros ya que se repetia con el arreglo fuera de la funcion

--- estudio.js
+++ estudio.js
@@ -3,14 +3,14 @@ const numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,8,9,10];
 const encRepetidos = (arr) =>{
   let contar = [];
 
-  const numeros = {
-    repetidos:[]
+  const repetidos = {
+    num:[]
   }
 
   for(i = 0; i<arr.length ; i++){
     if(contar.includes(arr[i])){
 
-      numeros.repetidos.push(arr[i]);
+      repetidos.num.push(arr[i]);
 
     }
     else{
@@ -18,7 +18,7 @@ const encRepetidos = (arr) =>{
     }
   };
 
-  return numeros
+  return repetidos
 };
 
 console.log(encRepetidos(numeros))
